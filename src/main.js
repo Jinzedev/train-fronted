1fc05f7d0188e9c8b8aa5d2db728c2338699cc09
槿泽 <1791209985@qq.com>
2023-07-29 20:11:33 +0800

    添加axios拦截器，用来拦截请求与返回信息，更好的观察接口信息

--- src/main.js
+++ src/main.js
@@ -11,3 +11,19 @@ const app = createApp(App)
 app.use(Antd).use(store).use(router).mount('#app');
 
 axios.defaults.baseURL = 'http://localhost:8000'
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(request =>{
+    console.log('请求参数：', request);
+    return request;
+},error => {
+    return Promise.reject(error);
+})
+axios.interceptors.response.use(response=>{
+    console.log('返回参数', response);
+    return response;
+},error => {
+    return Promise.reject(error);
+})
